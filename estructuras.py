da7a520b66b47579e68fb2e12e79ef859122f091
Alan Dri <alandri33@gmail.com>
2023-08-15 12:04:30 -0300

    estructuras y operadores

---
+++ estructuras.py
@@ -0,0 +1,21 @@
+age = int(input("Ingrse su edad por favor:"))
+
+if age < 18:
+    print("Es menor de edad")
+else:
+    print("Es mayore de edad")
+
+numero = 0
+while numero < 10:
+    print(numero)
+    numero += numero
+
+array = []
+
+for indice in array:
+    print(indice)
+
+#arranco en 1 hasta 20 y sumo de a uno
+for indice in range(1, 20, 1):
+    print(indice)
+
---
+++ operadores.py
@@ -0,0 +1,9 @@
+#puertas logicas
+
+#and or y not
+
+5<6 and 5<7
+
+8>6 or 8<5
+
+not 8 == 6
