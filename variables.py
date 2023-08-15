86dcde8a89666e09d7ea4933af048c3a9dac5bbe
Alan Dri <alandri33@gmail.com>
2023-08-15 10:31:02 -0300

    var

---
+++ variables.py
@@ -0,0 +1,32 @@
+x = 12.5
+y = 10
+z = False
+
+#char y string son el mismo tipo en python
+#es indistinto usar comilla doble o simple
+a = 'c'
+b = 'hola'
+c = "hola comillas dobles"
+
+#imprimimos el valor de la variable y el tipo
+print(x)
+print(type(x))
+
+print(y)
+print(type(y))
+
+print(z)
+print(type(z))
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+exp = 2**3
+
+print('Dos elevado al cubo es: ' + str(exp))
