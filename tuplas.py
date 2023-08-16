b995d985a7ecb112f8c4ef96654df4203c1b3f90
Alan Dri <alandri33@gmail.com>
2023-08-16 13:37:56 -0300

    tuplas

--- tuplas.py
+++ tuplas.py
@@ -1,4 +1,9 @@
 #tuplas
 
 #es un concepto muy relacionado a las listas
+#son inmutables, no se pueden modificar despues de su creacion
 
+tupla = (1, 2, 3, 4)
+ss
+#otra forma de crearla
+tupla2 = tuple((1, 2, 3, 4, 5))
