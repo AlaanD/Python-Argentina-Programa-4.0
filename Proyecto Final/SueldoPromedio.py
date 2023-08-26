11ab1ddfd156b07df735eba359aae6a17bc2ef23
Alan Dri <alandri33@gmail.com>
2023-08-26 11:58:19 -0300

    Primer punto proyecto final python

--- Proyecto Final/SueldoPromedio.py
+++ Proyecto Final/SueldoPromedio.py
@@ -13,3 +13,26 @@ c. Sueldo mejor de lo normal: más de 900 dólares.
 Tip: se debe utilizar estructuras condicionales.
 """
 
+class SueldoPromedio:
+    
+    def calcularPromedio(sueldo_pri_6m, sueldo_sig_4m, sueldo_ult_2m):
+        total = sueldo_pri_6m + sueldo_sig_4m + sueldo_ult_2m
+        resultado = total / 12
+        return round(resultado, 2)
+
+    def calcularRangoSalario(promedio):
+        if(promedio < 300):
+            return "bajo"
+        elif(promedio >= 300 and promedio <= 900):
+            return "normal"
+        return "mejor de lo normal"
+
+    sueldo_pri_6m = 300 * 6
+    sueldo_sig_4m = 500 * 4
+    sueldo_ult_2m = 700 * 2
+
+    promedio = calcularPromedio(sueldo_pri_6m, sueldo_sig_4m, sueldo_ult_2m)
+    rango = calcularRangoSalario(promedio)
+
+    print("El salario promedio de Juan es " + str(promedio))
+    print("Juan se encuentra cobrando un salario " + rango)
